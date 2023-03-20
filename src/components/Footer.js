0fa068f21d7bccef6101c37dfd58294c87243b55
Behzod <89260874+behzodk@users.noreply.github.com>
2023-03-20 13:44:51 +0500

    contact footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -25,26 +25,26 @@ function Footer(){
             </div>
             
             <div className="bottom">
-                <div>
-                    <h4>Project</h4>
-                    <a href="/">Changelog</a>
-                    <a href="/">Status</a>
-                    <a href="/">License</a>
-                    <a href="/">All versions</a>
-                </div>
-                <div>
-                    <h4>Community</h4>
-                    <a href="/">GitHub</a>
-                    <a href="/">Issues</a>
-                    <a href="/">Project</a>
-                    <a href="/">Twitter</a>
-                </div>
-                <div>
-                    <h4>Help</h4>
-                    <a href="/">Support</a>
-                    <a href="/">Troubleshooting</a>
-                    <a href="/">Contact Us</a>
-                </div>
+//                 <div>
+//                     <h4>Project</h4>
+//                     <a href="/">Changelog</a>
+//                     <a href="/">Status</a>
+//                     <a href="/">License</a>
+//                     <a href="/">All versions</a>
+//                 </div>
+//                 <div>
+//                     <h4>Community</h4>
+//                     <a href="/">GitHub</a>
+//                     <a href="/">Issues</a>
+//                     <a href="/">Project</a>
+//                     <a href="/">Twitter</a>
+//                 </div>
+//                 <div>
+//                     <h4>Help</h4>
+//                     <a href="/">Support</a>
+//                     <a href="/">Troubleshooting</a>
+//                     <a href="/">Contact Us</a>
+//                 </div>
                 <div>
                     <h4>Contact</h4>
                     <a href="mailto:contact@jizzakhps.uz">contact@jizzakhps.uz</a>
