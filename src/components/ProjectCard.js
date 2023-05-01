1c5c99ac2b105a381a31b63f4cda71ab3181b6d2
Behzod <89260874+behzodk@users.noreply.github.com>
2023-05-01 13:21:49 +0500

    .

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -2,6 +2,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { Card, Image, Stack, Heading, Text, Divider, CardBody, CardFooter } from '@chakra-ui/react'
 import Col from 'react-bootstrap/Col';
 import './StaffViewStyles.css';
+import LocalPhoneIcon from '@mui/icons-material/LocalPhone';
 
 function ProjectCard({id, full_name, date_of_birth, phone_number, role, desc, position, image}) {
   return (
@@ -16,8 +17,9 @@ function ProjectCard({id, full_name, date_of_birth, phone_number, role, desc, po
             <Stack mt='6' spacing='3'>
               <Heading size='md'>{full_name}</Heading>
               <Text>Born in {date_of_birth}</Text>
-              <Text>Contact Number: {phone_number}</Text>
               <Text>{desc}</Text>
+              <Divider />
+              <Text><LocalPhoneIcon/>Contact Number: {phone_number}</Text>
             </Stack>
           </CardBody>
           <Divider />
